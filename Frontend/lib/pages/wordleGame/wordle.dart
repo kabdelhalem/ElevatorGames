793503073bb06e5d05bb8b67d093ff199a5d39c9
kabdelhalem <kareem.abdelhalem@gmail.com>
2022-04-17 03:16:29 -0400

    Created List Widget Generator for vertical Text Field Forms

--- Frontend/lib/pages/wordleGame/wordle.dart
+++ Frontend/lib/pages/wordleGame/wordle.dart
@@ -2,6 +2,7 @@ import 'package:elevatorgames/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:elevatorgames/services/chooseDifficulty.dart';
+import 'package:elevatorgames/services/calculateWordle.dart';
 
 class Wordle extends StatefulWidget {
   
@@ -28,37 +29,42 @@ class _WordleState extends State<Wordle> {
           Text('Dropdown Value: ${chooseDifficulty(widget.newDropDownValue)}'),
           Padding(
             padding: const EdgeInsets.fromLTRB(250, 10, 250, 0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Row(
-                    children: List<Widget>.generate(chooseDifficulty(widget.newDropDownValue), (index){
-                          return Container(
-                            child: new Flexible(
-                              child: TextFormField(
-                                decoration: textInputDecoration,
-                                maxLength: 1,
-                                maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                                validator: (val){
-                                  return val!.length < 1 ? '' : null;
-                                },
-                                onChanged: (val){
-                                  setState(() {
-                                    myList.add(val);
-                                  });
-                                }
-                              ),
-                            ),
-                          );
-                        }),
-                  ),
-                ),
-              ],
+            child: Column(
+              children: List<Widget>.generate(6, (index){
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: List<Widget>.generate(chooseDifficulty(widget.newDropDownValue), (index){
+                              return Container(
+                                child: new Flexible(
+                                  child: TextFormField(
+                                    decoration: textInputDecoration,
+                                    maxLength: 1,
+                                    maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                                    validator: (val){
+                                      return val!.length < 1 ? '' : null;
+                                    },
+                                    onChanged: (val){
+                                      setState(() {
+                                        myList.add(val);
+                                      });
+                                    }
+                                  ),
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                );
+              }),
             ),
           ),
           ElevatedButton(
                   onPressed: (){
                     print(myList);
+                    //calculateWordle(myList);
                   },
                   child: Text(
                     'Enter',
